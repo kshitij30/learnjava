242ede884e1794b523c526091cc22f555fe37de1
Kshitij Panghaal <Kshitij@Kshitijs-MacBook-Air.local>
2018-11-02 16:42:53 +0530

    identation

--- gcd.java
+++ gcd.java
@@ -1,24 +1,22 @@
 import java.util.Scanner;
 
-class gcd{
-
+class gcd {
 	public static int gcd1(int m,int n){
 		int a=0,temp=0,i,r=0;
 		if (m ==0 ){
 			return n;
-		}
-		else if (n == 0) {
+		} else if (n == 0){
 			return m;
 		}
-	while(true){
-		r= m%n;
-	if(r==0){
-		return n;
+		while(true){
+			r= m%n;
+			if(r==0){
+				return n;
+			}
+			m=n;
+			n=r;
+		}
 	}
-	m=n;
-	n=r;
-}
-}
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
@@ -27,5 +25,5 @@ class gcd{
 		a = input.nextInt();
 		b = input.nextInt();
 		System.out.println("The GCD of "+a+" and "+b+" is "+gcd1(a,b));
-}
+	}
 }
